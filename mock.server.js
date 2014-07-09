85f7ae8b64a78db3d68e964154fba1df8e2674a2
fadrizul <fadrizul@gmail.com>
2014-07-10 03:30:01 +0800

    (FEAT) Make mock server able to run in multiple processes

--- mock.server.js
+++ mock.server.js
@@ -3,7 +3,7 @@
 * Author: Fadrizul Hasani <fadrizul@grabtaxi.com>
 * Date: 2014-07-10 02:40:27
 * Modified by: fadrizul
-* Modified time: 2014-07-10 03:00:22
+* Modified time: 2014-07-10 03:28:27
 */
 
 (function () { 'use strict';
@@ -24,5 +24,32 @@
     res.send(200);
   });
 
-  app.listen(7777);
+  // Enabling clustering
+  var cluster = require('cluster');
+  var http = require('http');
+  var numCPUs = require('os').cpus().length;
+
+  if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', function(deadWorker, code, signal) {
+      // Restart the worker
+      var worker = cluster.fork();
+
+      // Note the process IDs
+      var newPID = worker.process.pid;
+      var oldPID = deadWorker.process.pid;
+
+      // Log the event
+      console.log('worker ' + oldPID + ' died.');
+      console.log('worker ' + newPID + ' born.');
+    });
+  } else {
+    // Workers can share any TCP connection
+    app.listen(7777);
+    console.log("Server is now listening on port " + 7777);
+  }
 }).call(this);
